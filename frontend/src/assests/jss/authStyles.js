24c66fd4630083464f3e16b638ae0f701905c8d5
ikabir21 <ichankabir@gmail.com>
2021-10-28 12:52:54 +0530

    eslint, prettier and husky setup

--- frontend/src/assests/jss/authStyles.js
+++ frontend/src/assests/jss/authStyles.js
@@ -1,14 +1,14 @@
 const authStyles = (theme) => ({
-	root: {
-		flexGrow: 1,
-		height: "100vh",
-	},
-	container: {
-		padding: "40px",
-		marginTop: theme.spacing(2),
-	},
+  root: {
+    flexGrow: 1,
+    height: '100vh',
+  },
+  container: {
+    padding: '40px',
+    marginTop: theme.spacing(2),
+  },
   link: {
-    cursor: "pointer",
-  }
+    cursor: 'pointer',
+  },
 })
 export default authStyles
